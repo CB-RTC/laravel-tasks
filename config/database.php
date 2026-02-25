07a27b9fc102f89473f147740be8049541529983
CB-RTC <colton@raven-tech.ca>
2026-02-25 04:58:15 +0000

    change port not username, dummy

--- config/database.php
+++ config/database.php
@@ -140,9 +140,9 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
-            'username' => env('AZURE_REDIS_USERNAME'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
             'scheme' => 'tls',
         ],
